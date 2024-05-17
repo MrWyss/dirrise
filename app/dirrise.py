26693e10065672dab53516e1362ce8fc84065c05
MrWyss <marius.wyss@gmail.com>
2024-05-17 20:26:56 +0200

    chore: bump version to 1.1.2

--- app/dirrise.py
+++ app/dirrise.py
@@ -8,7 +8,7 @@ from apprise import AppriseConfig, Apprise, AppriseAsset, NotifyType
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-version = '1.1.1'
+version = '1.1.2'
 
 
 class CustomArgumentParser(argparse.ArgumentParser):
